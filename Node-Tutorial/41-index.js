92982114336d8b617f75317d89a430eb7a6e6871
Ajay Singh <117065425+ajaysingh266@users.noreply.github.com>
2023-05-06 07:07:25 +0530

    Update 41-index.js

--- Node-Tutorial/41-index.js
+++ Node-Tutorial/41-index.js
@@ -3,7 +3,7 @@ require("./41-config");
 const Product = require("./41-product");
 
 const app = express();
-app.use(express.json());
+app.use(express.json());            // body ko parse karna padta hai, means string to  json ke ander convert karna padta hai
 app.post("/create", async(req, res)=>{
     //     console.log(req.body);
     // res.sendFile("Done")
